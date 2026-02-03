4393f21a8a4ea23b66cad871a8b30d8e794fa037
Vlad Z <vladoszolotoi@gmail.com>
2026-02-03 22:23:26 +0300

    SecondCommit

--- src/test/java/ru/netology/javaqa/javamvn/services/VacationServiceTest.java
+++ src/test/java/ru/netology/javaqa/javamvn/services/VacationServiceTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-//import org.junit.jupiter.api.Test;
-//import ru.netology.javaqa.VacationService;
 
 public class VacationServiceTest {
 
@@ -16,9 +14,7 @@ public class VacationServiceTest {
     public void testVacationMonthsAtIncome10_000(int expected, int expense, int threshold) {
         VacationService service = new VacationService();
 
-        //int expected = 3;
         int actual = service.calcMonths(10_000, 3_000, 20_000);
-        //System.out.println(" Месяцев отдыха " + result1);
         Assertions.assertEquals(expected, actual);
     }
 
@@ -28,31 +24,10 @@ public class VacationServiceTest {
     })
     public void testVacationMonthsAtIncome100_000(int expected, int expense, int threshold) {
         VacationService service = new VacationService();
-
-        //int expected = 3;
         int actual = service.calcMonths(100_000, 6_000, 150_000);
-        //System.out.println(" Месяцев отдыха " + result1);
         Assertions.assertEquals(expected, actual);
     }
 
-//    @Test
-//    public void testVacationMonthsAtIncome100_000() {
-//        VacationService service = new VacationService();
-//
-//        int expected = 4;
-//        int actual = service.calcMonths(100_000, 6_000, 150_000);
-//        Assertions.assertEquals(expected, actual);
 
-//    }
-//
-//
-//        @Test
-//        public void testVacationMonthsAtIncome150_000 () {
-//        VacationService service = new VacationService();
-//
-//        int expected = 6;
-//        int actual = service.calcMonths(150_000, 50_000, 50_000);
-//        Assertions.assertEquals(expected, actual);
-//    }
 }
 
